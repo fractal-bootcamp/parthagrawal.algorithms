9a020272d227a7e27ab6ca56d3b0d4edccb1d72a
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-24 11:59:11 -0400

    fixed to remove stepmetadata and instead introduce another state variable for sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,38 @@ export default function Home() {
   const startArr = [3, 4, 5, 6, 7, 8, 1, 9, 2, 10]
   // const { arr } = bubbleSort(startArr)
   const [arr, setArr] = useState<number[]>(structuredClone(startArr))
-  const [stepMetadata, setStepMetadata] = useState<{
-    step: number;
-    swapped: boolean
-  }>({
-    swapped: false,
-    step: 0
+  const [sorted, setSorted] = useState(false);
+  const [step, setStep] = useState(0);
+  // const [stepMetadata, setStepMetadata] = useState<{
+  //   step: number;
+  //   swapped: boolean
+  // }>({
+  //   swapped: false,
+  //   step: 0
 
-  })
+  // })
 
   useEffect(() => {
-    const { newArr, swapped } = bubbleSortStep(arr, stepMetadata.step)
-    const { step } = stepMetadata;
+    const { newArr, swapped } = bubbleSortStep(arr, step)
 
     if (step < arr.length) {
       setArr(newArr);
+      if (swapped) setSorted(false);
 
       setTimeout(() => {
-        setStepMetadata({ step: step + 1, swapped: stepMetadata.swapped || swapped })
+        setStep(step + 1)
       }, 50)
     } else {
-      if (stepMetadata.swapped) {
-        setStepMetadata({ step: 0, swapped: false })
+      if (!sorted) {
+        setSorted(true)
+        setStep(0)
       }
     }
 
 
     // if arr.length && swapped == true restart
 
-  }, [stepMetadata]) // arr as dependency? 
+  }, [step]) // arr as dependency? 
 
   return (
     <>
